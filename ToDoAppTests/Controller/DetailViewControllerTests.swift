4c8c8ecbe4891e9f9c9cc83e288ea40730a9803e
Ihor <56516292+degster55@users.noreply.github.com>
2022-11-22 21:38:13 +0200

    Tested labels on DetailViewController

--- ToDoAppTests/Controller/DetailViewControllerTests.swift
+++ ToDoAppTests/Controller/DetailViewControllerTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import CoreLocation
 @testable import ToDoApp
 
 final class DetailViewControllerTests: XCTestCase {
@@ -37,8 +38,39 @@ final class DetailViewControllerTests: XCTestCase {
         XCTAssertTrue(sut.dateLabel.isDescendant(of: sut.view))
     }
     
+    func testHasLocationLabel() {
+        XCTAssertNotNil(sut.locationLabel)
+        XCTAssertTrue(sut.locationLabel.isDescendant(of: sut.view))
+    }
+    
     func testHasMapView() {
         XCTAssertNotNil(sut.mapView)
         XCTAssertTrue(sut.mapView.isDescendant(of: sut.view))
+    }
+    
+    func setupTaskAndAppearanceTransition() {
+        let coordinate = CLLocationCoordinate2D(latitude: 49.85910026206016, longitude: 24.01840198787776)
+        let location = Location(name: "Lviv", coordinate: coordinate)
+        let date = Date(timeIntervalSince1970: 1669142903)
+        let task = Task(title: "Qwerty", description: "desc", date: date, location: location )
+        sut.task = task
+        
+        sut.beginAppearanceTransition(true, animated: true)
+        sut.endAppearanceTransition()
+    }
+    
+    func testSettingTaskSetsTitleLabel() {
+        setupTaskAndAppearanceTransition()
+        XCTAssertEqual(sut.titleLabel.text, "Qwerty")
+    }
+    
+    func testSettingTaskSetsDescriptionLabel() {
+        setupTaskAndAppearanceTransition()
+        XCTAssertEqual(sut.descriptionLabel.text, "desc")
+    }
+    
+    func testSettingTaskSetsLocationLabel() {
+        setupTaskAndAppearanceTransition()
+        XCTAssertEqual(sut.locationLabel.text, "Lviv")
     }
 }
