f3b8f558e09274d2d1a1a4f998803e3b4c1188c8
Ihor <56516292+degster55@users.noreply.github.com>
2022-12-08 15:29:54 +0200

    Add NewTaskButton test

--- ToDoApp/Controller/TaskListViewController.swift
+++ ToDoApp/Controller/TaskListViewController.swift
@@ -12,6 +12,11 @@ class TaskListViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     @IBOutlet var dataProvider: DataProvider!
     
+    @IBAction func addNewTask(_ sender: UIBarButtonItem) {
+        if let viewController = storyboard?.instantiateViewController(withIdentifier: String(describing: NewTaskViewController.self)) as? NewTaskViewController {
+            present(viewController, animated: true)
+        } 
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ToDoAppTests/Supporting Files/ToDoAppTests.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by Ігор Ляхович on 08.11.2022.
+//
+
+import XCTest
+@testable import ToDoApp
+
+final class ToDoAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testInitialViewControllerIsTaskListViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        let rootViewController = navigationController.viewControllers.first as! TaskListViewController
+        
+        XCTAssertTrue(rootViewController is TaskListViewController)
+    }
+
+}
